e822142f5932aef219d58f60e108f39c65454a27
nunzio03 <nunzio03@hotmail.it>
2019-05-16 20:11:58 +0200

    Now we read H2O and Oil Temperatures and send them Via MQTT in a JSON

--- CanQuery.py
+++ CanQuery.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
-import can 
+import can
+import json
 
 
 def on_message(client, userdata, msg):
@@ -9,9 +10,12 @@ def on_message(client, userdata, msg):
 
         if frame.arbitration_id == int(msg.payload.decode("utf-8")):
             # stringa = "{}: {:02X} {:02X}".format(frame.arbitration_id, frame.data[0], frame.data[1])
-            stringa =  frame.data[0]*256+frame.data[1]
-            print(stringa)
-            client.publish("speaker", stringa)
+            tH2O = frame.data[0]*256+frame.data[1]
+            tOil = frame.data[2]*256+frame.data[3]
+            json_dict = {"tH2O": tH2O, "tOil": tOil}
+            json_string = json.dumps(json_dict)
+            print(json_string)
+            client.publish("speaker", json_string)
             break
 
 
@@ -31,6 +35,7 @@ client.on_message = on_message
 # client.username_pw_set("Rettorato", "Nucleare")
 
 # client.connect("broker.shiftr.io", 1883, 160)
+
 client.connect("localhost", 1883, 160)
 client.subscribe('listener')
 
