9435495bbd45c14632accfe48dc8892a944645bd
Nunzio Barone <nunzio03@hotmail.it>
2019-07-31 23:11:32 +0200

    Update CanQuery.py

--- CanQuery.py
+++ CanQuery.py
@@ -6,16 +6,16 @@ import json
 def on_message(client, userdata, msg):
     bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
     print("user requests id: ", int(msg.payload.decode("utf-8")), hex(int(msg.payload.decode("utf-8"))))
-    for frame in bus:
+    for frame in bus: #READING BUS
 
-        if frame.arbitration_id == int(msg.payload.decode("utf-8")):
+        if frame.arbitration_id == int(msg.payload.decode("utf-8")): #FINDING REQUESTED ID
             # stringa = "{}: {:02X} {:02X}".format(frame.arbitration_id, frame.data[0], frame.data[1])
-            tH2O = frame.data[0]*256+frame.data[1]
+            tH2O = frame.data[0]*256+frame.data[1]  #CALCULATING BINARY
             tOil = frame.data[2]*256+frame.data[3]
             json_dict = {"tH2O": tH2O, "tOil": tOil}
             json_string = json.dumps(json_dict)
             print(json_string)
-            client.publish("speaker", json_string)
+            client.publish("speaker", json_string) #SENDING DATA
             break
 
 
